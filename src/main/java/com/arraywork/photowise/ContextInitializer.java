62c87493ee1ec240b2a1c2027762a40a479e8383
metadream <app@zerg.cc>
2024-07-07 12:22:54 +0800

    Update ContextInitializer.java

--- src/main/java/com/arraywork/photowise/ContextInitializer.java
+++ src/main/java/com/arraywork/photowise/ContextInitializer.java
@@ -3,9 +3,10 @@ package com.arraywork.photowise;
 import java.io.File;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
@@ -18,15 +19,15 @@ import jakarta.servlet.ServletContextListener;
 @Component
 public class ContextInitializer implements ServletContextListener {
 
-    // storage directory (includes database and thumbnails)
-    @Value("${photowise.storage}")
-    private String storage;
+    @Resource
+    private Environment env;
 
-    // Create the directory required
+    // Create the storage directory required (and it must before JPA startup)
     // Auto-started order: Static Block -> ServletContextListener -> @PostConstruct
     // However, static blocks cannot get the configuration parameters
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        String storage = env.getProperty("photowise.storage");
         File dir = Path.of(storage, "thumbnails").toFile();
         if (!dir.exists()) dir.mkdirs();
     }
