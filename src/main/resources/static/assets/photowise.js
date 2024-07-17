39d0ab81dbd23d491722332cd34aaa990bf5dfec
metadream <app@zerg.cc>
2024-07-17 18:09:25 +0800

    Update photowise.js

--- src/main/resources/static/assets/photowise.js
+++ src/main/resources/static/assets/photowise.js
@@ -1,7 +1,7 @@
 import PhotoSwipeLightbox from '/assets/photoswipe-lightbox@5.4.4.js';
 import PhotoSwipeVideo from '/assets/photoswipe-video@1.0.2.js';
 
-// 元数据侧边栏
+// 照片信息侧边栏
 class ExifSidebar {
     static width = '300px';
 
@@ -25,6 +25,7 @@ class ExifSidebar {
     }
 }
 
+// 照片墙初始化
 export function initPhotoSwipe() {
     // 勾选元素捕获 Photoswipe 点击缩略图事件以防止传播
     const checkboxes = document.querySelectorAll('.photo-wall input[type="checkbox"]');
@@ -41,23 +42,25 @@ export function initPhotoSwipe() {
         loop: false,
         zoom: false,
         wheelToZoom: true,
+
+        // 图片视口根据容器大小适配（侧边栏带来的调整）
         getViewportSizeFn: function(options, pswp) {
-            if (pswp.element) {
-                const rect = pswp.element.getBoundingClientRect();
-                return {
-                    x: rect.width,
-                    y: window.innerHeight
-                };
-            }
+            if (!pswp.element) return;
+            const rect = pswp.element.getBoundingClientRect();
+            return {
+                x: rect.width,
+                y: window.innerHeight
+            };
         }
     });
     
+    // 关闭时恢复容器大小并隐藏侧边栏
     lightbox.on('close', () => {
-        const { element } = lightbox.pswp;
-        element.style.right = '0';
+        lightbox.pswp.element.style.right = '0';
         ExifSidebar.hide();
     });
 
+    // 自定义UI组件
     lightbox.on('uiRegister', function() {
         // 添加图片数据指示
         lightbox.pswp.ui.registerElement({
@@ -95,12 +98,14 @@ export function initPhotoSwipe() {
             html: '<svg class="icon info"><use xlink:href="/icons/icons.svg#info"></use></svg>',
 
             onInit: (el, pswp) => {
+                // 根据侧边栏调整容器大小
                 el.onclick = () => {
                     pswp.element.style.width = 'auto';
                     pswp.element.style.right = ExifSidebar.width;
                     pswp.updateSize();
                     ExifSidebar.show();
                 }
+                // 设置侧边栏字段数据
                 pswp.on('change', () => {
                     const { photoId } = pswp.currSlide.data.element.dataset;
                     console.log("photoId=", photoId);
